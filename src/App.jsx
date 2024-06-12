93c4632596e6c554ad49a98cda87dece12b17780
elvisuriel <urielmarciales@gmil.com>
2024-06-12 15:10:10 -0500

    subir repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { ChatClient } from './components/chat-client/ChatClient';
+
+
+function App() {  
+
+  return (
+    <div className="App"> 
+      <h1>Q´huboChat</h1>     
+      <ChatClient />
+    </div>
+  )
+}
+
+export default App
